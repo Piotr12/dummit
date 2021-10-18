713a8eb6d4fa05092c8a8ca55c48fea165aa54a1
Piotr12 <>
2021-10-18 19:54:22 +0200

    some renaming not to cause confusion what 'tests' are ... as they are actually just 'demos'

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import dummit as d
 
-with open('sample_test_library.yaml', 'r') as file:
+with open('demos/sample_test_library.yaml', 'r') as file:
     tests = d.TestLibrary(file.read(), d.TextLogger()) 
     tests.run("DEV")
     #tests.run("QA")
